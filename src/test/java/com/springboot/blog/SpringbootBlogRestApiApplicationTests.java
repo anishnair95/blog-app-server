b90dd878416c85ddd7097c4c2c05c305e12f0ccb
Anish Nair <anishnair95@gmail.com>
2023-07-30 19:10:51 +0530

    remove test annotation

--- src/test/java/com/springboot/blog/SpringbootBlogRestApiApplicationTests.java
+++ src/test/java/com/springboot/blog/SpringbootBlogRestApiApplicationTests.java
@@ -3,7 +3,6 @@ package com.springboot.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringbootBlogRestApiApplicationTests {
 
     @Test
