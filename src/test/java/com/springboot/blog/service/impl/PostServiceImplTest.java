c823ee0b988eab56755cd4ef7c746d820fc044ea
Anish Nair <anishnair95@gmail.com>
2023-12-25 19:51:41 +0530

    test: add blog app test case

--- src/test/java/com/springboot/blog/service/impl/PostServiceImplTest.java
+++ src/test/java/com/springboot/blog/service/impl/PostServiceImplTest.java
@@ -2,15 +2,21 @@ package com.springboot.blog.service.impl;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.springboot.blog.dto.PostDto;
 import com.springboot.blog.dto.PostResponse;
+import com.springboot.blog.entity.Category;
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.repository.CategoryRepository;
 import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.util.DataConvertor;
 import com.springboot.blog.util.MockDataGenerator;
 import org.junit.jupiter.api.Assertions;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,7 +26,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceImplTest {
@@ -83,8 +93,44 @@ class PostServiceImplTest {
         Assertions.assertEquals(10, postResponse.getPageSize());
     }
 
-//    @Test
+    @Test
     void createPost() {
+        // TODO: use argument captor learnt in java-wf in this
+        // since it's a mock so everything is null in the instance
+        PostDto postDto = Mockito.mock(PostDto.class);
+        when(postDto.getCategoryId()).thenReturn(2L);
+        when(postDto.getDescription()).thenReturn("test description");
+
+
+        // well here we are creating a new instance of Post using dto
+        Post mockPost = DataConvertor.postDtoToEntity(postDto);
+        mockPost.setId(1L);
+
+        ArgumentCaptor<Long> idCaptor = ArgumentCaptor.forClass(Long.class);
+        ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
+        when(categoryRepository.findById(idCaptor.capture())).thenReturn(Optional.ofNullable(Mockito.mock(Category.class)));
+        when(postRepository.save(postArgumentCaptor.capture())).thenReturn(mockPost);
+
+        PostDto savedPost = postService.createPost(postDto);
+
+        // getting the value that was passed in function
+        Long val = idCaptor.getValue();
+        // captured arguments verification
+        Assertions.assertEquals(2L, val);
+        Assertions.assertEquals("test description", postArgumentCaptor.getValue().getDescription());
+
+        // check invocation happened atleast 1 times
+        verify(postRepository, times(1)).save(any(Post.class));
+        Assertions.assertNotNull(savedPost);
+        Assertions.assertNull(savedPost.getCategoryId());
+        Assertions.assertEquals(1, savedPost.getId());
+    }
+
+    @Test
+    public void trialRun() {
+        Category category = Mockito.mock(Category.class);
+        when(category.getId()).thenReturn(2L);
+        postService.testRun(category);
     }
 
     @Test
